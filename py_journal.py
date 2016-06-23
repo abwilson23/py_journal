be6e9e57b392b13ca25a3862ac2238a1c4136a36
Andrew Wilson <tzinquaw.bp@gmail.com>
2016-06-22 21:52:07 -0700

    initial commit

---
+++ py_journal.py
@@ -0,0 +1,18 @@
+import os.path as os
+
+
+# Finds monthly journal tex file in the journal directory.
+def find_file():
+    
+# If no such file is found, create a new one for the current month. 
+# Should only do this at the beginning of each month, or the first
+# time that a journal entry is added in a new month.
+def create_new_file(): 
+
+
+# Add a new journal entry to the monthly journal
+def add_entry():
+
+
+if __name__ == "__main__":
+
