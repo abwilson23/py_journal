12997b6b179943b9d24a37e3f1385ebafc7d1a55
Andrew Wilson <tzinquaw.bp@gmail.com>
2016-06-24 00:29:29 -0700

    added template, and file checking

--- py_journal.py
+++ py_journal.py
@@ -1,8 +1,13 @@
 import os.path as os
+from shutil import copyfile
+import datetime
 
 
 # Finds monthly journal tex file in the journal directory.
-def find_file():
+def check_file(name):
+
+    if not os.path.isfile(name):
+        copyfile('template.tex', name + '.tex')
     
 # If no such file is found, create a new one for the current month. 
 # Should only do this at the beginning of each month, or the first
@@ -11,8 +16,21 @@ def create_new_file():
 
 
 # Add a new journal entry to the monthly journal
-def add_entry():
+def add_entry(header, f):
 
+    begin_env = "\\begin{entry}{\\today}{" + title + "}"
+
+# Returns the current month
+def get_date():
+    time = datetime.date.now()
+    return time.strftime("%B%Y")
 
 if __name__ == "__main__":
 
+    cur_name = get_date + '.tex'
+    check_file(cur_name)
+
+    with open(cur_name) as f:
+
+    
+    
