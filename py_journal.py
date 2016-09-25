c49313c3e7d64e22e6400289831e5eb7513df484
Andrew Wilson <awilson@artemis.localdomain>
2016-09-25 14:59:50 -0700

    improved user input

--- py_journal.py
+++ py_journal.py
@@ -14,7 +14,12 @@ def user_input(filename):
     
     date = input("Date: ") or get_day_for_entry()
     header = input("Header: ") 
-    entry = input("Entry: ")
+
+    entry = []
+    line = input("Entry: ")
+    while line != 'end':
+        entry.append(line + "\n")
+        line = input("-----> ")
     # bike_info = input("Bike distance: ")
 
     if date == "yesterday":
@@ -25,12 +30,13 @@ def user_input(filename):
 # Add a new journal entry to the monthly journal
 def add_entry(header, date, entry, filename):
 
-    begin_env = "\\begin{entry}{" + date + "}{" + header + "}"
-    end_env = "\\end{entry}\n\n\\end{document}"
+    begin_env = "\\begin{entry}{" + date + "}{" + header + "}\n"
+    end_env = "\n\\end{entry}\n\n\\end{document}"
     with open(filename, 'r+') as f:
         content = f.readlines()
         content[-1] = begin_env
-        content.append(entry)
+        for l in entry:
+            content.append(l)
         content.append(end_env)
         f.seek(0)
         
